1655236ab9ecce2b9d4468075441340adea51676
Douglas Hemken <hemken@ADS.SSC.WISC.EDU>
2017-05-12 11:00:31 -0500

    initial commit, files as scripts

---
+++ R/sasengine_output.r
@@ -0,0 +1,25 @@
+engine_output <- function (options, code, out, extra = NULL) 
+{
+  if (!is.logical(options$echo)) 
+    code = code[options$echo]
+  if (length(code) != 1L) 
+    code = paste(code, collapse = "\n")
+  if (options$engine == "sas" && 
+      length(out) > 1L && 
+      !grepl("[[:alnum:]]", out[2])) 
+    out = tail(out, -3L)
+  if (options$engine == "sashtml") {
+    #print(sum(grepl("nbsp", out)))
+    out <- gsub("&nbsp;", " ", out)
+  }
+  if (length(out) != 1L) 
+    out = paste(out, collapse = "\n")
+  out = sub("([^\n]+)$", "\\1\n", out)
+  options$engine = switch(options$engine, mysql = "sql", node = "javascript", 
+                          psql = "sql", Rscript = "r", options$engine)
+  paste(c(if (length(options$echo) > 1L || options$echo) 
+    (knitr::knit_hooks$get("source"))(code, options), 
+    if (options$results != "hide" && !knitr:::is_blank(out)) {
+      if (options$engine == "highlight") out else knitr:::wrap.character(out, options)
+    }, extra), collapse = "\n")
+}
---
+++ R/saslogengine.r
@@ -0,0 +1,39 @@
+knitr::knit_engines$set(saslog=function (options)
+{
+  code = {
+    f = basename(tempfile(pattern="sas", tmpdir=".", fileext=".sas"))
+    writeLines(c("OPTIONS NONUMBER NODATE PAGESIZE = MAX FORMCHAR = '|----|+|---+=|-/<>*' FORMDLIM=' ';title;",
+                 options$code), f)
+    on.exit(unlink(f))
+    listf = sub("[.]sas$", ".lst", f)
+    logf = sub("[.]sas$", ".log", f)
+    on.exit(unlink(c(logf, listf, f)), add = TRUE)
+    f
+  }
+  
+  code = paste(code, options$engine.opts)
+  cmd = options$engine.path
+  out = if (options$eval) {
+    message("running: ", cmd, " ", code)
+    tryCatch(system2(cmd, code, stdout = TRUE, stderr = TRUE,
+                     env = options$engine.env), error = function(e) {
+                       if (!options$error)
+                         stop(e)
+                       paste("Error in running command", cmd)
+                     })
+  }
+  else ""
+  if (!options$error && !is.null(attr(out, "status")))
+    stop(paste(out, collapse = "\n"))
+  if (options$eval &&  file.exists(listf))
+    out.listing = c(readLines(listf), out) else out.listing=""
+  if (options$eval &&  file.exists(logf))
+    out.log = c(readLines(logf), out)
+  out.log <- out.log[-(1:grep("FORMDLIM", out.log))]
+  out.log <- out.log[1:(grep("SAS Institute Inc.", out.log)-2)]
+  #commandlines <- grep("^[[:digit:]]", out.log)
+  #if (length(commandlines)>0) {out.log <- out.log[-commandlines]}
+  
+  engine_output(options, out.log, out.listing)
+}
+)
