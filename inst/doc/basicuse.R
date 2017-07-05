5e6d0fc509e5f9d42248a829d7c8469eab7c8b7b
Hemken <hemken@ADS.SSC.WISC.EDU>
2017-07-05 13:38:45 -0500

    Change "examples" to "vignettes"

---
+++ inst/doc/basicuse.R
@@ -0,0 +1,14 @@
+## ----setup, message=FALSE------------------------------------------------
+require(SASmarkdown)
+
+if (file.exists("C:/Program Files/SASHome/SASFoundation/9.4/sas.exe")) {
+  saspath <- "C:/Program Files/SASHome/SASFoundation/9.4/sas.exe"
+} else if (file.exists("D:/SASHome/SASFoundation/9.4/sas.exe")) {
+  saspath <- "D:/SASHome/SASFoundation/9.4/sas.exe"
+} else {
+  saspath <- "sas"
+}
+
+sasopts <- "-nosplash -ls 75"
+
+
