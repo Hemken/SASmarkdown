9069e818a8f7fd9a53f96cae0236ca869229e457
Douglas Hemken <dehemken@wisc.edu>
2022-08-13 06:02:24 -0500

    CRAN cleanup

--- R/find_sas.r
+++ R/find_sas.r
@@ -26,7 +26,7 @@ find_sas <- function(message=TRUE) {
       # SAS is not found at default location in windows
       # Try to find it from PATH
       sasexe <- Sys.which("sas")
-      attr(sasexe, "names") <- NULL    # remove attr so this is plain text like other branch
+      attr(sasexe, "names") <- NULL    # remove attr so this is plain text
       if (nchar(sasexe) > 0) {
           if (message) message("SAS found at ", sasexe)
       } else {
@@ -48,11 +48,40 @@ find_sas <- function(message=TRUE) {
     }
   }
 } else if (.Platform$OS.type == "unix") {
-    sasexe <- system2("which", "sas", stdout=TRUE)
-    if (message) message("SAS found at ", sasexe)
+    # sasexe <- system2("which", "sas", stdout=TRUE)
+    # if (message) message("SAS found at ", sasexe)
+    sasexe <- Sys.which("sas")
+    if (nchar(sasexe) > 0) {
+        if (message) message("SAS found at ", sasexe)
+        attr(sasexe, "names") <- NULL    # remove attr so this is plain text
+    } else {
+        sasexe <- NULL    # change the 0-length `sasexe` back to `NULL`
+    }
+    if (is.null(sasexe)){
+        for (d in paste(c("/usr/local/sas","/software/sas"),
+                        "SASFoundation", sep="/")) {
+            if (dir.exists(d)) {
+                for (v in versionlist) {
+                    dv <- paste(d,v, sep="/")
+                    if (dir.exists(dv)) {
+                        dvf <- paste(dv, "sas", sep="/")
+                        if (file.exists(dvf)) {
+                            sasexe <- dvf
+                            if (message) message("SAS found at ", sasexe)
+                            break
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
 } else {
-  message("SAS executable not found.\n Specify the location of your SAS executable.")
-    message("  In SAS, issue the command ` %put %sysget(SASROOT); ` to find this.")
+    message("Unknown OS.\n Specify the location of your SAS executable.")
 }
+    if (is.null(sasexe)) {
+        message("Specify the location of your SAS executable with `knitr::opts_chunk$set`.")
+        message("In SAS, issue the command ` %put %sysget(SASROOT); ` to find this.")
+    }
   return(sasexe)
 }
