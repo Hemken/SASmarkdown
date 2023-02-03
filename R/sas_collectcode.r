136eef0975b85332f88e16b3c37176ff595d17bb
Douglas Hemken <hemken@ads.ssc.wisc.edu>
2023-02-03 08:18:24 -0600

    Add sashtml5 engine to collectcode

--- R/sas_collectcode.r
+++ R/sas_collectcode.r
@@ -8,7 +8,9 @@ sas_collectcode <- function() {
     
 knitr::knit_hooks$set(collectcode = function(before, options, envir) {
     if (!before) {
-        if (options$engine %in% c("sas", "saslog", "sashtml", "sashtmllog")) {
+        if (options$engine %in% c("sas", "saslog", 
+                                  "sashtml", "sashtmllog", 
+                                  "sashtml5", "sashtml5log")) {
             autoexec <- file("autoexec.sas", open="at")
             writeLines(options$code, autoexec)
             close(autoexec)
