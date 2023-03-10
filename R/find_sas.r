d0b8e724065f2b68e0d3cb819bd5192a0bada3a2
Douglas Hemken <hemken@ads.ssc.wisc.edu>
2023-03-10 08:37:14 -0600

    package startup, for CRAN

--- R/find_sas.r
+++ R/find_sas.r
@@ -80,8 +80,8 @@ find_sas <- function(message=TRUE) {
     message("Unknown OS.\n Specify the location of your SAS executable.")
 }
     if (is.null(sasexe)) {
-        message("Specify the location of your SAS executable with `knitr::opts_chunk$set`.")
-        message("In SAS, issue the command ` %put %sysget(SASROOT); ` to find this.")
+        packageStartupMessage("Specify the location of your SAS executable with `knitr::opts_chunk$set`.\n",
+          "In SAS, issue the command ` %put %sysget(SASROOT); ` to find this.")
     }
   return(sasexe)
 }
