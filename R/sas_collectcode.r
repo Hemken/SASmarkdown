21ba018fc3fd0a76e17745d820a100dcb001c068
Douglas Hemken <hemken@ads.ssc.wisc.edu>
2023-11-28 12:35:48 -0600

    Update for knitr 1.45

--- R/sas_collectcode.r
+++ R/sas_collectcode.r
@@ -15,16 +15,20 @@ knitr::knit_hooks$set(collectcode = function(before, options, envir) {
             autoexec <- file("autoexec.sas", open="at")
             writeLines(options$code, autoexec)
             close(autoexec)
-            #print(sys.frames())
-            #print(sys.calls())
-        do.call("on.exit", 
-            list(quote(unlink("autoexec.sas")), add=TRUE),
-            envir = sys.frame(-9)) 
-        do.call("on.exit", 
-           list(quote(print(sys.call())), add=TRUE),
-            envir = sys.frame(-9)) 
-            # sys.frame(1) or sys.frame(-10) is rmarkdown::render()
-            # sys.frame(-9) is knitr::knit()
+            # print(sys.calls())
+            # print(sys.frames())
+        if (utils::packageVersion('knitr') < '1.45') {
+            do.call("on.exit",
+                    list(quote(unlink("autoexec.sas")), add=TRUE),
+                    envir = sys.frame(-9))
+        } else if (utils::packageVersion('knitr') >= '1.45') {
+            do.call("on.exit",
+                    list(quote(unlink("autoexec.sas")), add=TRUE),
+                    envir = sys.frame(-10))
+            # sys.frame(1) or sys.frame(-11) is rmarkdown::render()
+            # sys.frame(2) or sys.frame(-10) is knitr::knit()
+        }
+
         if (!is.null(oautoexec)) {
             do.call("on.exit",
                 list(quote(writeLines(oautoexec, "autoexec.sas")), add=TRUE),
